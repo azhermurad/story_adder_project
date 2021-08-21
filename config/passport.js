c4be104691e9fbb640e7665b2bb1db4887a26066
azher ali <azhermurad@gmial.com>
2021-08-21 23:26:44 +0500

    proxy issuse solve

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,8 @@ module.exports = (passport) => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/auth/google/callback"
+        callbackURL: "/auth/google/callback",
+        proxy: true
     },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id }).then((user) => {
